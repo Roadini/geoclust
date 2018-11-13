fe01cf5d1e6ab0f724545ad49996c69c8689dcc6
tiago ramalho <t.ramalho22@ua.pt>
2018-11-13 11:20:51 +0000

    docker compose and pupulate

---
+++ populate.sh
@@ -0,0 +1,3 @@
+curl --header "Content-Type: application/json" --request POST --data '{"user_id":1,"list_name":"Restaurante Croacia"}' http://localhost:3001/api/v1/lists
+curl --header "Content-Type: application/json" --request POST --data '{"user_id":1,"list_name":"Monumentos em Londres"}' http://localhost:3001/api/v1/lists
+curl --header "Content-Type: application/json" --request POST --data '{"user_id":1,"list_name":"Igrejas em Roma"}' http://localhost:3001/api/v1/lists
