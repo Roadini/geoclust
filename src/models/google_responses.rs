32c6f923dec1709d4d0785e05f2717a9413c571e
Luís Silva <lfssilva@ua.pt>
2018-11-26 11:25:53 +0000

    Add image fetching

--- src/models/google_responses.rs
+++ src/models/google_responses.rs
@@ -1,14 +1,37 @@
 use models::gspots::{NewGSpot};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct DebugLog {
-    pub line: Vec<String>
+pub struct Photos {
+    pub height: i32,
+    pub html_attributions: Vec<String>,
+    pub photo_reference : String,
+    pub width: i32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Point {
+    pub lat: f32,
+    pub lng: f32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Viewport {
+    pub northeast : Point,
+    pub southwest : Point
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Geometry {
+    pub location : Point,
+    pub viewport : Viewport
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Candidate {
     pub formatted_address: String,
+    pub geometry: Geometry,
     pub name: String,
+    pub photos : Option<Vec<Photos>>,
     pub place_id: String,
     pub types: Vec<String>
 }
@@ -26,12 +49,21 @@ impl Candidate{
             name: from.name.as_str().to_string(),
             address: from.formatted_address.as_str().to_string(),
             google_place_id: from.place_id.as_str().to_string(),
-            lat: 40.3,
-            lng: -8.4,
+            lat: from.geometry.location.lat,
+            lng: from.geometry.location.lng,
             primary_type: from.types[0].as_str().to_string(),
             secondary_type: "Odeio a minha vida".to_string()
         };
 
         new_gspot
+    }
+
+    pub fn get_photo_id(from: &Candidate) -> String{
+        match from.photos {
+            // The division was valid
+            Some(ref x) => x[0].photo_reference.as_str().to_string(),
+            // The division was invalid
+            None    => "error".to_string(),
+        }
     }
 }
--- src/routes/gspots.rs
+++ src/routes/gspots.rs
@@ -8,6 +8,11 @@ use models::gspots::{GSpot};
 use models::google_responses::{Candidate, GoogleResponse};
 use serde_json::Value;
 
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
 #[get("/gspots", format = "application/json")]
 pub fn index(conn: DbConn) -> Json<Value> {
     let gspots = GSpot::all(&conn);
@@ -32,17 +37,18 @@ pub fn get_possible_gspots( lat: f32, lng: f32, conn: DbConn) -> Json<Value> {
 
 #[get("/gspots/populate?<lat>&<lng>", format = "application/json")]
 pub fn populate_by_coord(lat: f32, lng: f32, conn: DbConn) -> Json<Value> {
+
     println!("Coordenadas. Lat: {} Lng: {}",lat, lng);
+
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("set DATABASE_URL");
 
 
     let request_url = format!("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=*\
             &inputtype=textquery\
-            &fields=formatted_address,name,place_id,types\
+            &fields=formatted_address,geometry,id,name,permanently_closed,photos,place_id,types\
             &locationbias=circle:50@{},{}\
             &key={}",lat, lng,api_key);
-
 
     let mut response = reqwest::get(&request_url)
         .expect("Que treta");
@@ -52,16 +58,76 @@ pub fn populate_by_coord(lat: f32, lng: f32, conn: DbConn) -> Json<Value> {
 
     for x in &gr.candidates {
         let mut candidate_name = x.name.as_str();
+        let mut inserted_id : i32 = 0;
         println!("Adding token Candidate ... {}", candidate_name);
-        GSpot::insert(Candidate::to_gspot(x), &conn);
-    }
+        inserted_id = GSpot::insert(Candidate::to_gspot(x), &conn)[0];
+
+
+        populate_photos_by_coord(Candidate::get_photo_id(x), inserted_id);
+    };
 
     Json(json!({
         "status": 200,
-        "result": format!("{:#?}",gr.candidates[0].name),
     }))
 }
 
+pub fn populate_photos_by_coord(photo_id: String, place_internal_id: i32) {
+
+    if(photo_id == "error"){
+        return;
+    }
+
+    println!("photo_id : {} place_internal_id: {}", photo_id, place_internal_id);
+
+    dotenv().ok();
+    let api_key = env::var("API_KEY").expect("set DATABASE_URL");
+
+    let request_url = format!("https://maps.googleapis.com/maps/api/place/photo? \
+            &photoreference={}\
+            &maxwidth=400\
+            &key={}",photo_id, api_key);
+
+    let mut response = reqwest::get(&request_url)
+        .expect("Que treta");
+
+    // TODO:: FIX
+    //let mut extension = "jpeg".to_string();
+    //if response.status().is_success() {
+
+    //    let len = response.headers()["CONTENT_TYPE"].to_str().unwrap();
+
+    //    println!("{}",len);
+
+    //    let splits :Vec<&str> = len.split("/").collect();
+
+    //    extension = splits[1].to_string();
+    //}
+
+    let file_name = format!("public/{}.{}", place_internal_id.to_string(), extension);
+
+    let path = Path::new(&file_name);
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("Couldn't create File"),
+
+        Ok(file) => file,
+    };
+
+    let mut buffer = Vec::new();
+
+    match response.read_to_end(&mut buffer){
+        Err(why) => panic!("Something went Wrong Reading Response"),
+
+        Ok(_) => println!("Successfully read Response"),
+    };
+
+    match file.write_all(&buffer) {
+        Err(why) => panic!("Couldn't write to File"),
+
+        Ok(_) => println!("Successfully wrote to File"),
+    };
+}
 
 #[get("/gspots/<id>", format = "application/json")]
 pub fn show(conn: DbConn, id: i32) -> Json<Value> {
