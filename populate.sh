9a6b818797792e44f169d7a024eb905ebadae53b
tiago ramalho <t.ramalho22@ua.pt>
2018-12-04 01:34:13 +0000

    improve populate script

--- populate.sh
+++ populate.sh
@@ -1,2 +1,11 @@
-curl --request GET \
-    http://localhost:3001/api/v1/gspots/populate?lat=40.633125&lng=-8.659492
+curl "http://172.22.0.7:3001/api/v1/gspots/populate?lat=40.633160&lng=-8.659393"
+
+curl "http://172.22.0.7:3001/api/v1/gspots/populate?lat=40.633125&lng=-8.659492"
+
+curl "http://172.22.0.7:3001/api/v1/gspots/populate?lat=40.634069&lng=-8.648356"
+
+curl "http://172.22.0.7:3001/api/v1/gspots/populate?lat=40.638033&lng=-8.651053"
+
+curl "http://172.22.0.7:3001/api/v1/gspots/populate?lat=40.638375&lng=-8.657370"
+
+curl "http://172.22.0.7:3001/api/v1/gspots/populate?lat=40.639474&lng=-8.650897"
